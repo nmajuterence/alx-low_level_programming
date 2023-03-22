23c618acc42009a10c3a27f979f6a4b420f12f81
nmajuterence <nmajuterence@gmail.com>
2023-03-22 20:41:22 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+	{
+		putchar(b);
+		if (b != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
