3766903ed08163b7ba57e4c39b665087fedb4287
nmajuterence <nmajuterence@gmail.com>
2023-03-22 18:46:57 +0000

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
 
 int main(void)
 {
-		char i;
+		char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+		int i;
 
-		for (i = 'a'; i <= 'z'; i++)
+		for (i = 0; i < 26; i++)
 {
-			putchar(i);
+			putchar(letters[i]);
+			putchar('\n');
 }
 		return (0);
 }
