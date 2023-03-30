58ac233dd6fa4db69d32b508108ac9ef328a3092
nmajuterence <nmajuterence@gmail.com>
2023-03-30 01:27:17 +0000

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the value of n to 98
+ *@n: is the value to be changed
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
