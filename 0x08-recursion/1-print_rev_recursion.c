0990e240aa38122aa92888d449761858c4194040
nmajuterence <nmajuterence@gmail.com>
2023-04-10 16:15:24 +0000

    function fix -  prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,18 @@
 
 /**
  * _print_rev_recursion - this function prints a string in reverse
- *
  * @s: function parameter
- *
  * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-		_print_rev_recursion(s + 1);
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
 	_putchar(*s);
 }
