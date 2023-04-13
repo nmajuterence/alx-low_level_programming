f7adb9a9bb6cc1cb4fb17a3f712be40b1cd8458a
nmajuterence <nmajuterence@gmail.com>
2023-04-13 01:26:57 +0000

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - this program multiplies two numbers
+ * @argc: counter
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
