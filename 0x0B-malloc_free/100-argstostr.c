7493a30d3452f2cf7988d116a7602d39bd4c3859
nmajuterence <nmajuterence@gmail.com>
2023-04-14 05:05:23 +0000

    hot fix concatenates all arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * argstostr - this function concatenates all the arguments
+ * @ac: count
+ * @av: vector
+ *
+ * Return: s
+ */
+char *argstostr(int ac, char **av)
+{
+	int _cat = 0, x = 0, y = 0, z = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (x < ac)
+	{
+		while (av[x][y])
+		{
+			_cat++;
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+
+	s = malloc((sizeof(char) * _cat) + ac + 1);
+
+	x = 0;
+	while (av[x])
+	{
+		while (av[x][y])
+		{
+			s[z] = av[x][y];
+			z++;
+			y++;
+		}
+		s[z] = '\n';
+
+		y = 0;
+		z++;
+		x++;
+	}
+	z++;
+	s[z] = '\0';
+	return (s);
+}
