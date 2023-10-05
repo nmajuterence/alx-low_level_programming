8317fd2c44527d909882c0b71b9727d7c1fb9265
Nmajuterence <nmajuterence@gmail.com>
2023-10-05 03:55:56 +0100

    variables and constants

--- w3schools/changevariablevalues.c
+++ w3schools/changevariablevalues.c
@@ -8,10 +8,27 @@
 
 int main(void)
 {
-	int x = 5;
-	int y = 6;
-	int sum = x + y;
+	//create variables
+	
+	int myNum = 5;
+	float myFloatNum = 5.99;
+	char myLetter = 'D';
+	float sum = (float) 5 / 2;
+	int sum1 = 100 + 50;
+	int sum2 = sum1 + 250;
+	int sum3 = sum1 + sum2;
 
-	printf("%d\n", sum);
+	//print variables
+
+	printf("%d\n", myNum);
+	printf("%f\n", myFloatNum);
+	printf("%c\n", myLetter);
+	printf("%.1f\n", myFloatNum);
+	printf("%.2f\n", myFloatNum);
+	printf("%f\n", sum);
+	printf("%d\n", sum1);
+	printf("%d\n", sum2);
+	printf("%d\n", sum3);
+
 	return (0);
 }
