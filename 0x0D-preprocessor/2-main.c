481682320675ff1e75cdbea911bcacb7f30e101d
Nmajuterence <nmajuterence@gmail.com>
2023-10-12 02:22:11 +0100

    Write a program that prints the name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main - prints filename
+ * main - prints the name of the file
  *
  * Return: Always 0.
 */
+
 int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
